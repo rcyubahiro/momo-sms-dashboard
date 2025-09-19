8078cc2d831c5c8696cf02a60e1f5b2d097c6f62
ingdia <d.ingabire2@alustudent.com>
2025-09-19 10:36:26 +0200

    adding table of users

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS momo_sms;
 USE momo_sms;
+
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Unique user identifier',
+    name VARCHAR(50) NOT NULL COMMENT 'User full name',
+    phone_number VARCHAR(15) NULL UNIQUE COMMENT 'User phone number',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Account creation timestamp'
+);
